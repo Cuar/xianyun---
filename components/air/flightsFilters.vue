c38210e578684b6b7019fa3a9b4ade3c66a224c0
Cuar <46847886+Cuar@users.noreply.github.com>
2020-02-19 05:21:46 +0800

    多重选择

--- components/air/flightsFilters.vue
+++ components/air/flightsFilters.vue
@@ -64,6 +64,9 @@
                 撤销
     		</el-button>
         </div>
+        <!-- 渲染空的字符串，只需要监听功能 -->
+        <!-- <span>{{filter}}</span> -->
+        <span>{{filter}}</span>
     </div>
 </template>
 
@@ -91,50 +94,92 @@ export default {
     methods: {
         // 选择机场时候触发
         handleAirport(value){
-            const newData =this.data.flights.filter(v =>{
-                return v.org_airport_name ===value
-            })
-            this.$emit('getData',newData)
+            // const newData =this.data.flights.filter(v =>{
+            //     return v.org_airport_name ===value
+            // })
+            // this.$emit('getData',newData)
         },
 
         // 选择出发时间时候触发
         handleFlightTimes(value){
-            const arr =value.split(',');
-            const newData =this.data.flights.filter(v =>{
-                //Number() 函数把对象的值转换为数字
-                const hours = Number(v.dep_time.split(":")[0]);
+            // const arr =value.split(',');
+            // const newData =this.data.flights.filter(v =>{
+            //     //Number() 函数把对象的值转换为数字
+            //     const hours = Number(v.dep_time.split(":")[0]);
 
-                  return Number(arr[0]) <= hours && hours < Number(arr[1]);
+            //       return Number(arr[0]) <= hours && hours < Number(arr[1]);
 
-            })
-            this.$emit("getData", newData);
+            // })
+            // this.$emit("getData", newData);
         },
 
          // 选择航空公司时候触发
         handleCompany(value){
             //循环取值 从里面找到符合条件value值
             //fliter 方法不懂 -> 箭头函数不懂
-            console.log(value)
-            const newData =this.data.flights.filter(v =>{
-                //如果return 的值是true ，说明符合条件
-                return v.airline_name ===value
-            })
-            this.$emit('getData',newData)
+            // console.log(value)
+            // const newData =this.data.flights.filter(v =>{
+            //     //如果return 的值是true ，说明符合条件
+            //     return v.airline_name ===value
+            // })
+            // this.$emit('getData',newData)
         },
 
          // 选择机型时候触发
         handleAirSize(value){
-             const newData = this.data.flights.filter(v => {
-            return v.plane_size === value;
-             })
-               this.$emit("getData", newData);
+            //  const newData = this.data.flights.filter(v => {
+            // return v.plane_size === value;
+            //  })
+            //    this.$emit("getData", newData);
         },
         
         // 撤销条件时候触发
         handleFiltersCancel(){
             
         },
-    }
+    },
+    computed:{
+       filter(){
+            const newData = this.data.flights.filter(v => {
+                // 假设当前的数据都是符合条件
+                let valid = true;
+
+                // 找到不符合条件的
+                if(this.company && v.airline_name !== this.company){
+                    valid = false;
+                }
+
+                // 机型大小
+                if(this.airSize && v.plane_size !== this.airSize){
+                    valid = false;
+                }
+
+                // 机场
+                if(this.airport && v.org_airport_name !== this.airport){
+                    valid = false;
+                }
+
+                // 时间
+                if(this.flightTimes){
+                    // 选中的时间段
+                    const arr = this.flightTimes.split(","); // ["6","12"]
+                    // 当前航班小时
+                    const hours = Number(v.dep_time.split(":")[0]);
+                    
+                    // 如果出发的小时不在时间段内，认为是不合法
+                    if(Number(arr[0]) > hours || hours >= Number(arr[1])){
+                        valid = false;
+                    }
+                }
+
+                return valid;
+            })
+
+            this.$emit("getData", newData);
+            return "";
+        }  
+    
+}
 }
 </script>
 
