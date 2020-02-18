f30d6bf9148d15cb4b76eaa62a64a7181d4db5e2
Cuar <46847886+Cuar@users.noreply.github.com>
2020-02-19 03:51:21 +0800

    备份/过滤/分页

---
+++ components/air/flightsFilters.vue
@@ -0,0 +1,143 @@
+<template>
+    <div class="filters">
+        <el-row type="flex" class="filters-top" justify="space-between" align="middle">
+            <el-col :span="8">
+                {{data.info.departCity}} - {{data.info.destCity}}/ {{data.info.departDate}} 
+            </el-col>
+            <el-col :span="4">
+                <el-select size="mini" v-model="airport" placeholder="起飞机场" @change="handleAirport">
+                    <el-option
+                    v-for='(item,index) in data.options.airport'
+                    :key='index'
+                    :label="item"
+                    :value="item"
+                    >
+                    </el-option>
+                </el-select>
+            </el-col>
+            <el-col :span="4">
+                <el-select size="mini" v-model="flightTimes"  placeholder="起飞时间" @change="handleFlightTimes">
+                    <el-option
+                     v-for="(item,index) in data.options.flightTimes"
+                    :key='index'
+                    :label="`${item.from}:00 - ${item.to}:00` "
+                    value="1"
+
+                    >
+                    </el-option>
+                </el-select>
+            </el-col>
+            <el-col :span="4">
+                <el-select size="mini" v-model="company"  placeholder="航空公司" @change="handleCompany">
+                    <el-option
+                   
+                    v-for='(item,index) in data.options.company'
+                    :key='index'
+                    :label="item"
+                    :value="item"
+                    >
+                    </el-option>
+                </el-select>
+            </el-col>
+            <el-col :span="4">
+                <el-select size="mini" v-model="airSize" placeholder="机型" @change="handleAirSize">
+                    <el-option
+                    
+                    v-for="(item,index) in airSizeList" 
+                    :key='index'
+                    :label="item.label"
+                    :value="item.value"
+                    
+                    >
+                    </el-option>
+                </el-select>
+            </el-col>
+        </el-row>
+        <div class="filter-cancel">
+            筛选：
+            <el-button 
+                       type="primary" 
+                       round 
+                       plain 
+                       size="mini" 
+                       @click="handleFiltersCancel">
+                撤销
+    		</el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        data:{
+            type:Object,
+            default:{}
+        }
+    },
+    data(){
+        return {
+            airport: "",        // 机场
+            flightTimes: "",    // 出发时间
+            company: "",        // 航空公司
+            airSize: "",        // 机型大小
+            airSizeList:[
+                {label:'大',value:'L'},
+                {label:'中',value:'M'},
+                {label:'小',value:'S'},
+            ]
+        }
+    },
+    methods: {
+        // 选择机场时候触发
+        handleAirport(value){
+            
+        },
+
+        // 选择出发时间时候触发
+        handleFlightTimes(value){
+            
+        },
+
+         // 选择航空公司时候触发
+        handleCompany(value){
+            //循环取值 从里面找到符合条件value值
+            //fliter 方法不懂 -> 箭头函数不懂
+            console.log(value)
+            const newData =this.data.flights.filter(v =>{
+                //如果return 的值是true ，说明符合条件
+                return v.airline_name ===value
+            })
+            this.$emit('getData',newData)
+        },
+
+         // 选择机型时候触发
+        handleAirSize(value){
+           
+        },
+        
+        // 撤销条件时候触发
+        handleFiltersCancel(){
+            
+        },
+    }
+}
+</script>
+
+<style scoped lang="less">
+    .filters{
+        margin-bottom:20px;
+    }
+
+    .filters-top{
+        > div{
+            /deep/ .el-select{
+                margin-left:10px;
+            }
+        }
+    }
+
+    .filter-cancel{
+        margin-top:10px;
+    }
+</style>
--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -6,7 +6,9 @@
             <div class="flights-content">
                 <!-- 过滤条件 -->
                 <div>
-                    
+                        <!-- 过滤条件 -->
+                        <!-- 把值传给组件 -->
+                <FlightsFilters :data='cacheFlightsData' @getData='getData' />
                 </div>
                 
                 <!-- 航班头部布局 -->
@@ -14,10 +16,21 @@
                 
                 <!-- 航班信息 -->
                 <FlightsItem
-                v-for="(item,index) in flightsData.flights"
+                v-for="(item,index) in dataList"
                 :key='index'
                 :data='item'
                 />
+                <!-- 分页 -->
+                <!-- 使用一个分页组件 -->
+                 <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pageIndex"
+      :page-sizes="[5, 10, 15, 20]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
             </div>
 
             <!-- 侧边栏 -->
@@ -32,16 +45,34 @@
 import FlightsListHead from "@/components/air/flightsListHead.vue"
 import FlightsItem from "@/components/air/flightsItem.vue"
 import moment from "moment";
+import FlightsFilters from "@/components/air/flightsFilters.vue"
 
 export default {
       components: {
-        FlightsListHead,FlightsItem 
+        FlightsListHead,FlightsItem ,FlightsFilters 
     },
     data(){
         return {
             // 机票总数据 （有info, flights, total, options这些属性）
-            flightsData: {}, // 航班总数据
-            dataList: []     // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
+            flightsData: {
+                flights:[],
+                info:{},
+                options:{}
+            },
+            //备份数据 反正修改
+            cacheFlightsData: {
+                
+                info:{},
+                options:{}
+            }, // 航班总数据
+           // dataList: []   ,  // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
+
+            //当前页数
+            pageIndex:1,
+            //当前条数
+            pageSize: 5,
+            //总条数
+            total:0,
         }
     },
     mounted(){
@@ -51,9 +82,48 @@ export default {
             params:this.$route.query
         }).then(res =>{
             //总数据
-            console.log(res)
+            
             this.flightsData =res.data
+            //备份数据，拷贝一份出来
+            this.cacheFlightsData ={...res.data}
+            console.log(res.data)
+            //修改总条数
+            this.total =this.flightsData.total
+
         })
+    },
+    methods:{
+            // 切换条数时触发
+         handleSizeChange(index) {
+        this.pageSize=index
+      },
+          // 切换页数时触发
+      handleCurrentChange(index) {
+        this.pageIndex =index
+      },
+      //获取过滤组件过滤后数据arr是数组
+      getData(arr){
+          this.flightsData.flights =arr
+          //总条数
+          this.total =arr.length;
+      }
+    },
+    computed:{
+        //计算属性函数内部引用实例（this）的属性一旦发生变化，函数就会重新执行返回新值
+          dataList(){
+            // 判断flightsData有没有值
+            if(!this.flightsData.flights){
+                // 没有值返回一个空数组
+                return [];
+            }
+
+            // 第一页是0-5，第二页是5-10，第三页是10-15
+            const arr = this.flightsData.flights.slice(
+                (this.pageIndex - 1) * this.pageSize, 
+                this.pageIndex * this.pageSize
+            );
+            return arr;
+        }
     }
 }
 </script>
